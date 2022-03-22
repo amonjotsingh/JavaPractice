49a8a8418f08e896dc68ff53f145d86e6f2a956e
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-22 13:27:45 -0400

    fill array

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,16 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] arr = {"Hello", "my friend", "how are you"};
-        List<String> list = Arrays.asList(arr);
-        for (String s : list) System.out.print(s + " ");
-
-
+    public static void main(String[] args){
+// ASLIST METHOD
+        //        String[] arr = {"Hello", "my friend", "how are you"};
+//        List<String> list = Arrays.asList(arr);
+//        for (String s : list) System.out.print(s + " ");
+//FILL THE ARRAY
+        int[] arr = new int[5];
+        Arrays.fill(arr, 100);
+        for (Integer i : arr)
+            System.out.print(i + " ");
     }
 
 }

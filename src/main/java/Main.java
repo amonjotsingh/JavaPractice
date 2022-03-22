a0c4a06fe797fa447d13f440d79109ec56cbef07
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-22 13:10:52 -0400

    asList

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"Hello", "my friend", "how are you"};
+        List<String> list = Arrays.asList(arr);
+        for (String s : list) System.out.print(s + " ");
+
+
+    }
+
+}

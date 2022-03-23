b0ee80bfa60d51afa1d24275c9fc6462a49e0b99
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-23 12:24:08 -0400

    Arraylist Mehtod

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,10 +24,12 @@ public class Main {
         System.out.println(String.valueOf(list));
         list.set(4, 4);
         System.out.println(list.get(5));
-        list.addAll(list);
         System.out.println(String.valueOf(list));
         System.out.println(list.indexOf(7));
         System.out.println("Last index of 4 is " + list.lastIndexOf(4));
-
+        Iterator itr = list.iterator();
+        while(itr.hasNext()){
+            System.out.print(itr.next());
+        }
     }
 }

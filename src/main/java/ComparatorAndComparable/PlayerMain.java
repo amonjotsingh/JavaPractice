e3705c09f753c8d5215e1b446eb35aafc0ba7f66
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-24 15:50:23 -0400

    Set

--- src/main/java/ComparatorAndComparable/PlayerMain.java
+++ src/main/java/ComparatorAndComparable/PlayerMain.java
@@ -1,26 +1,23 @@
 package ComparatorAndComparable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+
+import java.util.*;
 
 public class PlayerMain {
     public static void main(String[] args) {
 
-        List<Player> list = new ArrayList();
-        list.add(new Player("jot", 25));
-        list.add(new Player("Manu", 28));
-        list.add(new Player("Batman", 15));
-        System.out.println(list.toString());
+//        List<Player> list = new ArrayList();
+//        list.add(new Player("jot", 25));
+//        list.add(new Player("Manu", 28));
+//        list.add(new Player("Batman", 15));
+//        System.out.println(list.toString());
 
         Comparator<Player> com = new Comparator<Player>() {
             @Override
             public int compare(Player o1, Player o2) {
-                if (o1.getAge() > o2.getAge()) return 1;
-                else return -1;
+                return (o1.getName().compareTo(o2.getName()));
             }
         };
-        Collections.sort(list, com);
+//        Collections.sort(list, com);
         //Defining the comparator inside the calling func
 //        Collections.sort(list, (Player player1, Player player2) -> {
 //
@@ -28,7 +25,15 @@ public class PlayerMain {
 //            return 1;}
 //            else return -1;
 //        });
-        System.out.println(list.toString());
+//        System.out.println(list.toString());
+
+        Set<Player> set = new TreeSet<>();
+        set.add(new Player("jot", 25));
+        set.add(new Player("Manu", 28));
+        set.add(new Player("Batman", 15));
+        System.out.println(set.toString());
+
+
     }
 
 }

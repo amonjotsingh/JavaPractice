4cd5b016f0e216826cf16b512cf170f524e27717
chhinajot99@gmail.com <chhinajot99@gmail.com>
2022-03-27 04:49:30 -0400

    Iterating through Hashmap

--- src/main/java/ComparatorAndComparable/HashMapImp.java
+++ src/main/java/ComparatorAndComparable/HashMapImp.java
@@ -1,6 +1,9 @@
 package ComparatorAndComparable;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class HashMapImp {
     public static void main(String[] args) {
@@ -8,12 +11,20 @@ public class HashMapImp {
         map.put("batman", 70);
         map.put("superman", 80);
         System.out.println(map);
-        map.put("flash",100);
+        map.put("flash", 100);
         map.replace("superman", 80, 90);
         map.remove("aquaman");
-        map.put("martian",110);
+        map.put("martian", 110);
         System.out.println(map);
         System.out.println(map.get("batman"));
         System.out.println(map.containsKey("superman"));
+        Set<Map.Entry<String, Integer>> entrySet = map.entrySet();
+        Iterator<Map.Entry<String, Integer>> iterator = entrySet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
     }
+
 }
